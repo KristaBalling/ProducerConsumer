f62f5f2281e928698c8d25c445791c6a465af3f7
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-23 14:11:32 -0500

    Uses try, finally block to lock and unlock

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -48,8 +48,13 @@ class MyProducer implements Runnable {
             try {
                 System.out.println(color + "Adding..." + num);
                 bufferLock.lock();
-                buffer.add(num);
-                bufferLock.unlock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
+
+//                bufferLock.unlock();
 
                 Thread.sleep(random.nextInt(1000));
             } catch(InterruptedException e) {
@@ -59,8 +64,11 @@ class MyProducer implements Runnable {
 
         System.out.println(color + "Adding EOF and exiting...");
         bufferLock.lock();
-        buffer.add("EOF");
-        bufferLock.unlock();
+        try {
+            buffer.add("EOF");
+        }finally {
+            bufferLock.unlock();
+        }
     }
 }
 
@@ -76,24 +84,27 @@ class MyConsumer implements Runnable {
     }
 
     public void run() {
-        while (true) {
+        while(true) {
             bufferLock.lock();
-            synchronized (buffer) {
-                if (buffer.isEmpty()) {
-                    bufferLock.unlock();
-                    continue;
-                }
+            try {
+                    if (buffer.isEmpty()) {
+                        continue;
+                    }
                     if (buffer.get(0).equals(EOF)) {
                         System.out.println(color + "Exiting");
-                        bufferLock.unlock();
                         break;
                     } else {
                         System.out.println(color + "Removed " + buffer.remove(0));
                     }
+
+                } finally {
                     bufferLock.unlock();
                 }
             }
-
         }
+
     }
+
+
+
 
